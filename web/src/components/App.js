aff5cb2367d8b7a054c87e3ea94e78394a7baadb
martscastrillo <mart.castrillo@gmail.com>
2023-03-25 01:36:59 +0100

    add styles

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -16,18 +16,18 @@ const App = () => {
       if (response.success === true) {
         setDataForm(response.dataForm);
         // Si la usuaria introduce bien sus datos redireccionamos desde la página de login al inicio de la página
-       
+
       } else {
         // Si la usuaria introduce mal sus datos guardamos el error que nos devuelve el API para que se pinte en la página
-        /* setLoginErrorMessage(response.errorMessage);*/ 
-      } 
+        /* setLoginErrorMessage(response.errorMessage);*/
+      }
     });
     console.log('hola')
   };
 
   const handleInput = (ev) => {
     ev.preventDefault();
-    setDataForm({...dataForm, [ev.target.name]: ev.target.value});
+    setDataForm({ ...dataForm, [ev.target.name]: ev.target.value });
   };
   const handleSubmit = (ev) => {
     ev.preventDefault();
@@ -36,20 +36,23 @@ const App = () => {
   };
 
 
-    return(
-       <div>
-          <h1 className="h1">get in touch</h1>
-          <span className="line_red"></span>
-          <span className="line_blue"></span>
-      <form action="" onSubmit={handleSubmit}>
-        <input className ="input" name="name" type="text" placeholder="Nombre" value={dataForm.name} onChange={handleInput}/>
-        <input className ="input" name="email" type="email" placeholder="Email" value={dataForm.email} onChange={handleInput}/>
-        <input className ="input" name="phone" type="tlf" placeholder="Telefono" value={dataForm.phone} onChange={handleInput}/>
-        <input className ="input" name="message" type="text" placeholder="Mensaje" value={dataForm.message} onChange={handleInput}/>
-        <input type="submit" value="Submit"/>
-      </form>
-      </div>
-    )
+  return (
+    <div>
+      <div className="title">
+      <h1 className="h1">get in touch</h1>
+      <span className="line_red"></span>
+      <span className="line_blue"></span></div>
+      <form className="form" action="" onSubmit={handleSubmit}>
+        <div className="div">
+        <div className="div_input"><input className="input" name="name" type="text" placeholder="Nombre" value={dataForm.name} onChange={handleInput} /></div>
+        
+        <div className="div_input"><input className="input" name="email" type="email" placeholder="Email" value={dataForm.email} onChange={handleInput} /></div>
+        <div className="div_input"><input className="input" name="phone" type="tlf" placeholder="Telefono" value={dataForm.phone} onChange={handleInput} /></div>
+        <div className="div_input"><input className="input" name="message" type="text" placeholder="Mensaje" value={dataForm.message} onChange={handleInput} /></div></div>
+        <input className="submit" type="submit" value="Submit" />
+      </form >
+    </div >
+  )
 };
 
 export default App;
