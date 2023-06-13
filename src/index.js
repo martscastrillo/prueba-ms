66c37c49aebe575ca9253c71b6a80baa29a15a03
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-13 21:18:52 +0200

    add array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -10,35 +9,32 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
-  
+	console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
 let calcHistory = [];
-server.get('/calc', (req, res) => {
-    const args = req.query.args || []; 
-    const sum = args.reduce((acc, val) => {
-        const num = parseFloat(val);
-        if (!isNaN(num)) {
-          return acc + num;
-        } else {
-          return acc;
-        }
-      }, 0);
-    const calcResult = {
-        args: args,
-        sum: sum,
-        timestamp: new Date().toISOString()
-      };
-    
-      calcHistory.push(calcResult); 
+server.get("/calc", (req, res) => {
+	const args = req.query.args || [];
+	const sum = args.reduce((acc, val) => {
+		const num = parseFloat(val);
+		if (!isNaN(num)) {
+			return acc + num;
+		} else {
+			return acc;
+		}
+	}, 0);
+	const calcResult = {
+		args: args,
+		sum: sum,
+		timestamp: new Date().toISOString(),
+	};
 
-  console.log(args);
-  });
-  server.get('/history', (req, res) => {
-   
-    console.log(calcHistory);
-    res.json(calcHistory);
-  });
-const staticServer = './src/public-react';
+	calcHistory.push(calcResult);
+	console.log(args);
+});
+server.get("/history", (req, res) => {
+	console.log(calcHistory);
+	res.json(calcHistory);
+});
+const staticServer = "./src/public-react";
 server.use(express.static(staticServer));
--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,31 +1,92 @@
 import "../stylesheets/App.scss";
-
+import { useState } from "react";
 const App = () => {
+	const [dataArray, setDataArray] = useState([]);
+	const [inputValues, setInputValues] = useState({});
+  
+	const handleChange = (event) => {
+	  const { name, value } = event.target;
+	  setInputValues(prevInputValues => ({ ...prevInputValues, [name]: value }));
+	};
+  
+	
+	console.log(dataArray);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const valuesArray = Object.values(inputValues);
+		setDataArray(prevDataArray => [...prevDataArray, ...valuesArray]);
+	  
+		setInputValues({});
+	  console.log(dataArray);
+	  };
+	  const handleReset = (event) => {
+		event.preventDefault();
+		setDataArray([]);
+		setInputValues({});
+	  console.log(dataArray);
+	  };
 	return (
 		<div className="superbox">
-			<form action="" className="superbox__form">
+			<h1 className="superbox__h1">full stack test</h1>
+	 <form action="" className="superbox__form" onSubmit={handleSubmit}>
 				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="">value 1</label>
-					<input type="text" />
+					<label htmlFor="value1">value 1</label>
+					<input
+						id="value1"
+						placeholder="value 1"
+					
+          name="input1"
+          value={inputValues.input1|| ''}
+          onChange={handleChange}
+					/>
 				</fieldset>
 				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="">value 2</label>
-					<input type="text" />
+					<label htmlFor="value2">value 2</label>
+					<input
+						
+						id="value2"
+						placeholder="value 2"
+					
+          name="input2"
+          value={inputValues.input2|| ''}
+          onChange={handleChange}
+					/>
 				</fieldset>
 				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="">value 3</label>
-					<input type="text" />
+					<label htmlFor="value3">value 3</label>
+					<input
+						type="text"
+							id="value3"
+						placeholder="value 3"
+					
+          name="input3"
+          value={inputValues.input3|| ''}
+          onChange={handleChange}
+					/>
 				</fieldset>
 				<fieldset className="superbox__form--fieldset">
 					<label htmlFor="">value 4</label>
-					<input type="text" />
+					<input
+						type="text"
+						placeholder="value 4"
+							id="value4"
+						
+					
+          name="input4"
+          value={inputValues.input4|| ''}
+          onChange={handleChange}
+					/>
 				</fieldset>
-				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="">value 5</label>
-					<input type="text" />
-				</fieldset>
-				<input className="superbox__form--submit" type="submit" value="Submit" />
-			</form>
+<div className="extradiv">
+				<input
+					className="superbox__form--submit"
+					type="submit"
+					value="Submit"
+				/>
+				<button type="reset" className="superbox__form--submit" onClick={handleReset}>Reset</button></div>
+			</form> 
+			 
+	
 			<div className="superbox__results">
 				<h2>history results:</h2>
 				<h3>resultado</h3>
