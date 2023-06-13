556e6e2e427a8ea3e22c90d6be72bedeefaf2937
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-13 21:47:35 +0200

    reset hard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+
 
 // create and config server
 const server = express();
@@ -9,32 +10,18 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-	console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-let calcHistory = [];
-server.get("/calc", (req, res) => {
-	const args = req.query.args || [];
-	const sum = args.reduce((acc, val) => {
-		const num = parseFloat(val);
-		if (!isNaN(num)) {
-			return acc + num;
-		} else {
-			return acc;
-		}
-	}, 0);
-	const calcResult = {
-		args: args,
-		sum: sum,
-		timestamp: new Date().toISOString(),
-	};
 
-	calcHistory.push(calcResult);
-	console.log(args);
+server.post('/submit', (req, res) => {
+  const response = req.body;
+  console.log(response);
+  res.json(response);
+  
 });
-server.get("/history", (req, res) => {
-	console.log(calcHistory);
-	res.json(calcHistory);
-});
-const staticServer = "./src/public-react";
+
+const staticServer = './src/public-react';
 server.use(express.static(staticServer));
+
+
--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,96 +1,131 @@
-import "../stylesheets/App.scss";
+import apiData from "../services/api";
 import { useState } from "react";
+import "../stylesheets/App.scss";
+import Header from "./Header";
+import Form from "./Form";
+import CountrySection from "./CountrySection";
+import Footer from "./Footer";
+
 const App = () => {
-	const [dataArray, setDataArray] = useState([]);
-	const [inputValues, setInputValues] = useState({});
-  
-	const handleChange = (event) => {
-	  const { name, value } = event.target;
-	  setInputValues(prevInputValues => ({ ...prevInputValues, [name]: value }));
+	const [errorNameMessage, seterrorNameMessage] = useState("");
+	const [errorMailMessage, seterrorMailMessage] = useState("");
+	const [errorPhoneMessage, seterrorPhoneMessage] = useState("");
+	const [errorMessageMessage, seterrorMessageMessage] = useState("");
+	const [checkbox, setCheckbox] = useState(false);
+	const [hidden, setHidden] = useState("hidden");
+	const [active, setActive] = useState(false);
+	const [burger, setBurger] = useState("burger");
+	const [treatment, setTreatment] = useState("");
+	const [dataForm, setDataForm] = useState({
+		name: "",
+		email: "",
+		phone: "",
+		message: "",
+		treatment: "",
+	});
+
+	const sendFormApi = (data) => {
+		apiData.sendFormApi(data).then((response) => {
+			if (response) {
+				setDataForm(response.dataForm);
+			}
+		});
 	};
-  
-	
-	console.log(dataArray);
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const valuesArray = Object.values(inputValues);
-		setDataArray(prevDataArray => [...prevDataArray, ...valuesArray]);
-	  
-		setInputValues({});
-	  console.log(dataArray);
-	  };
-	  const handleReset = (event) => {
-		event.preventDefault();
-		setDataArray([]);
-		setInputValues({});
-	  console.log(dataArray);
-	  };
+	const handleBurger = (ev) => {
+		ev.preventDefault();
+		if (active === true) {
+			setActive(false);
+			setBurger("burger");
+		}
+		if (active === false) {
+			setActive(true);
+			setBurger("burger active");
+		}
+	};
+	const handleInput = (ev) => {
+		ev.preventDefault();
+		if (ev.target.name === "name") {
+			if (ev.target.value.length > 3) {
+				seterrorNameMessage("");
+			} else {
+				seterrorNameMessage("Name must be longer.");
+			}
+		}
+		if (ev.target.name === "email") {
+			if (/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/.test(ev.target.value)) {
+				seterrorMailMessage("");
+			} else {
+				seterrorMailMessage("Email must be formatted correctly.");
+			}
+		}
+		if (ev.target.name === "phone") {
+			if (
+				/^[(]?[0-9]{3}[)]?[-\s\\.]?[0-9]{3}[-\s\\.]?[0-9]{3}$/.test(
+					ev.target.value
+				)
+			) {
+				seterrorPhoneMessage("");
+			} else {
+				seterrorPhoneMessage("Phone must be formatted correctly.");
+			}
+		}
+		if (ev.target.name === "message") {
+			if (ev.target.value.length > 10) {
+				seterrorMessageMessage("");
+			} else {
+				seterrorMessageMessage("Message must be longer.");
+			}
+		}
+		setDataForm({ ...dataForm, [ev.target.name]: ev.target.value });
+	};
+
+	const handleChange = () => {
+		setCheckbox(!checkbox);
+		if (checkbox === true) {
+			setDataForm({ ...dataForm, checkbox: false });
+			setHidden("hidden");
+		}
+		if (checkbox === false) {
+			setDataForm({ ...dataForm, checkbox: true });
+			setHidden("");
+		}
+	};
+	const handleSubmit = (ev) => {
+		ev.preventDefault();
+		sendFormApi(dataForm);
+		resetForm();
+	};
+	const handleTreatment = (ev) => {
+		setTreatment(ev.target.value);
+		setDataForm({ ...dataForm, [ev.target.name]: ev.target.value });
+	};
+	const resetForm = () => {
+		setDataForm({ name: "", email: "", phone: "", message: "", treatment: "" });
+		setCheckbox(false);
+		setHidden("hidden");
+	};
 	return (
-		<div className="superbox">
-			<h1 className="superbox__h1">full stack test</h1>
-	 <form action="" className="superbox__form" onSubmit={handleSubmit}>
-				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="value1">value 1</label>
-					<input
-						id="value1"
-						placeholder="value 1"
-					
-          name="input1"
-          value={inputValues.input1|| ''}
-          onChange={handleChange}
-					/>
-				</fieldset>
-				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="value2">value 2</label>
-					<input
-						
-						id="value2"
-						placeholder="value 2"
-					
-          name="input2"
-          value={inputValues.input2|| ''}
-          onChange={handleChange}
-					/>
-				</fieldset>
-				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="value3">value 3</label>
-					<input
-						type="text"
-							id="value3"
-						placeholder="value 3"
-					
-          name="input3"
-          value={inputValues.input3|| ''}
-          onChange={handleChange}
-					/>
-				</fieldset>
-				<fieldset className="superbox__form--fieldset">
-					<label htmlFor="">value 4</label>
-					<input
-						type="text"
-						placeholder="value 4"
-							id="value4"
-						
-					
-          name="input4"
-          value={inputValues.input4|| ''}
-          onChange={handleChange}
-					/>
-				</fieldset>
-<div className="extradiv">
-				<input
-					className="superbox__form--submit"
-					type="submit"
-					value="Submit"
-				/>
-				<button type="reset" className="superbox__form--submit" onClick={handleReset}>Reset</button></div>
-			</form> 
-			 
-	
-			<div className="superbox__results">
-				<h2>history results:</h2>
-				<h3>resultado</h3>
-			</div>
+		<div>
+			<Header burger={burger} handleBurger={handleBurger}></Header>
+			<Form
+				handleSubmit={handleSubmit}
+				handleInput={handleInput}
+				errorNameMessage={errorNameMessage}
+				checkbox={checkbox}
+				handleChange={handleChange}
+				hidden={hidden}
+				dataFormEmail={dataForm.email}
+				dataFormPhone={dataForm.phone}
+				dataFormName={dataForm.name}
+				dataFormMessage={dataForm.message}
+				handleTreatment={handleTreatment}
+				treatment={treatment}
+				errorPhoneMessage={errorPhoneMessage}
+				errorMailMessage={errorMailMessage}
+				errorMessageMessage={errorMessageMessage}
+			></Form>
+			<CountrySection></CountrySection>
+			<Footer></Footer>
 		</div>
 	);
 };
