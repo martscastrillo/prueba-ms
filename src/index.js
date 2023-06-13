42d395e0fbff7bd4ed00cb14309fef4614ecc011
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-13 18:33:31 +0200

    add html & css

--- src/index.js
+++ src/index.js
@@ -11,18 +11,34 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+  
 });
 
 let calcHistory = [];
 server.get('/calc', (req, res) => {
-    const args = req.query.args;
+    const args = req.query.args || []; 
+    const sum = args.reduce((acc, val) => {
+        const num = parseFloat(val);
+        if (!isNaN(num)) {
+          return acc + num;
+        } else {
+          return acc;
+        }
+      }, 0);
+    const calcResult = {
+        args: args,
+        sum: sum,
+        timestamp: new Date().toISOString()
+      };
+    
+      calcHistory.push(calcResult); 
 
   console.log(args);
   });
   server.get('/history', (req, res) => {
    
     console.log(calcHistory);
- 
+    res.json(calcHistory);
   });
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,12 +1,35 @@
-
 import "../stylesheets/App.scss";
 
-
 const App = () => {
-	
 	return (
-		<div>
-			
+		<div className="superbox">
+			<form action="" className="superbox__form">
+				<fieldset className="superbox__form--fieldset">
+					<label htmlFor="">value 1</label>
+					<input type="text" />
+				</fieldset>
+				<fieldset className="superbox__form--fieldset">
+					<label htmlFor="">value 2</label>
+					<input type="text" />
+				</fieldset>
+				<fieldset className="superbox__form--fieldset">
+					<label htmlFor="">value 3</label>
+					<input type="text" />
+				</fieldset>
+				<fieldset className="superbox__form--fieldset">
+					<label htmlFor="">value 4</label>
+					<input type="text" />
+				</fieldset>
+				<fieldset className="superbox__form--fieldset">
+					<label htmlFor="">value 5</label>
+					<input type="text" />
+				</fieldset>
+				<input className="superbox__form--submit" type="submit" value="Submit" />
+			</form>
+			<div className="superbox__results">
+				<h2>history results:</h2>
+				<h3>resultado</h3>
+			</div>
 		</div>
 	);
 };
